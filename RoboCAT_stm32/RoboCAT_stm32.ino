5ce28f74749ae49d54c9baa8ca45cf70adf34928
ittonru <serg@itton.ru>
2019-08-05 18:18:57 +0800

    Update RoboCAT_stm32.ino

--- RoboCAT_stm32/RoboCAT_stm32.ino
+++ RoboCAT_stm32/RoboCAT_stm32.ino
@@ -4,7 +4,7 @@
  Serjey Kolobov (ittonru)
  August 2, 2019.
 */
-
+///3333
 #include <Servo.h>
 
 Servo s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13;  // create servo object to control a servo
